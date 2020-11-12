8d1f7e0a38039426b3b3002c0212ace0ee448266
Rohit Jaiswal <rht.jaiswl@gmail.com>
2020-11-12 13:55:18 +0530

    Add ReadMe

---
+++ build/precache-manifest.5d775bc6d3a53ce5c4a19c6009b7708e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41692f03ad01054f4de1aff0428ce411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c3cf484dcdbc1f63d3e",
+    "url": "/static/css/main.d7535a11.chunk.css"
+  },
+  {
+    "revision": "ae835104e3857208cf3b",
+    "url": "/static/js/2.6ec04bc2.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/static/js/2.6ec04bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3cf484dcdbc1f63d3e",
+    "url": "/static/js/main.9ccc6cc8.chunk.js"
+  },
+  {
+    "revision": "caf7e007f4a603ffbbfe",
+    "url": "/static/js/runtime-main.2a438b3c.js"
+  }
+]);
